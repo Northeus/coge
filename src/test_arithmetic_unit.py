a1155cfb051496f7bc543c25fe5cd151f29a9684
Northeus <dark2482000@gmail.com>
2025-06-09 15:41:05 +0200

    add coverage example

--- src/test_arithmetic_unit.py
+++ src/test_arithmetic_unit.py
@@ -56,7 +56,7 @@ async def generate_clock(dut: HierarchyObject, stop: asyncio.Event) -> None:
 
 @dataclass
 class Bin:
-    values: list[int]
+    values: range
     hits: int = 0
 
     def sample(self, value: int) -> bool:
@@ -105,14 +105,14 @@ def coverpoint(port: str) -> Coverpoint:
     return Coverpoint(port, [], [], [])
 
 
-def normal_bin(coverpoint: Coverpoint, values: list[int] | int) -> Bin:
-    bin = Bin([values] if isinstance(values, int) else values)
+def normal_bin(coverpoint: Coverpoint, values: range | int) -> Bin:
+    bin = Bin(range(values, values + 1) if isinstance(values, int) else values)
     coverpoint.bins.append(bin)
     return bin
 
 
-def ignore_bin(coverpoint: Coverpoint, values: list[int] | int) -> Bin:
-    bin = Bin([values] if isinstance(values, int) else values)
+def ignore_bin(coverpoint: Coverpoint, values: range | int) -> Bin:
+    bin = Bin(range(values, values + 1) if isinstance(values, int) else values)
     coverpoint.ignore_bins.append(bin)
     return bin
 
@@ -124,17 +124,18 @@ def sequence(coverpoint: Coverpoint, sequence: list[int]) -> Sequence:
 
 
 def cross(bin_groups: list[tuple[Coverpoint, list[Bin]]]) -> Cross:
+    # TODO: might remove duplicities in product...
     assert all(b in cp.bins for cp, bins in bin_groups for b in bins)
-    coverpoints = set(x[0] for x in bin_groups)
-    cross = set(product(*(x[1] for x in bin_groups)))
-    return Cross(list(coverpoints), list(cross), [0] * len(cross))
+    coverpoints = list(x[0] for x in bin_groups)
+    cross = list(product(*(x[1] for x in bin_groups)))
+    return Cross(coverpoints, cross, [0] * len(cross))
 
 
 def sample(values: dict[str, int],
            coverpoints: list[Coverpoint],
            crosses: list[Cross]
            ) -> None:
-    hits: set[Bin] = set()
+    hits_id: set[int] = set()
 
     for coverpoint in coverpoints:
         value = values[coverpoint.port]
@@ -148,12 +149,33 @@ def sample(values: dict[str, int],
 
         for bin in coverpoint.bins:
             if bin.sample(value):
-                hits.add(bin)
+                hits_id.add(id(bin))
 
     for cross in crosses:
         for i, bins in enumerate(cross.cross):
-            if all(b in hits for b in bins):
+            if all(id(b) in hits_id for b in bins):
                 cross.hits[i] += 1
+
+
+def print_coverage(coverpoints: list[Coverpoint],
+                   crosses: list[Cross]
+                   ) -> None:
+    print('[INFO] Coverage:')
+
+    for coverpoint in coverpoints:
+        print(f'\tCoverpoint [{coverpoint.port}]:')
+
+        for bin in coverpoint.bins:
+            print(f'\t\tBin {bin.values}: {bin.hits}')
+
+        for sequence in coverpoint.sequences:
+            print(f'\t\tSequence {sequence.sequence}: {sequence.hits}')
+
+    for cross in crosses:
+        print(f'\tCross {list(x.port for x in cross.coverpoints)}:')
+
+        for i, target in enumerate(cross.cross):
+            print(f'\t\tTarget {target}: {cross.hits[i]}')
 
 
 @cocotb.test
@@ -165,6 +187,20 @@ async def test_random_stimuli(dut: HierarchyObject) -> None:
     ports = {k: v for k, v in extract_ports(dut).items()
              if k in ('OP_CODE', 'MOVI', 'REG_A', 'REG_B', 'MEM', 'IMM')}
     stop = asyncio.Event()
+    log_transactions = False
+
+    # Define functional coverage.
+    coverpoints = [coverpoint('OP_CODE'), coverpoint('REG_A')]
+    for i in range(4):
+        normal_bin(coverpoints[0], i)
+    for i in range(4):
+        for j in range(4):
+            if i != j:
+                sequence(coverpoints[0], [i, j])
+    normal_bin(coverpoints[1], range(0, 2**30))
+    normal_bin(coverpoints[1], range(2**30, 2**31))
+    normal_bin(coverpoints[1], range(2**31, 2**32))
+    crosses = [cross([(cp, cp.bins) for cp in coverpoints])]
 
     # Start clock.
     await cocotb.start(generate_clock(dut, stop))
@@ -180,24 +216,29 @@ async def test_random_stimuli(dut: HierarchyObject) -> None:
     # Perform transactions.
     await RisingEdge(dut.CLK)
     for i in range(1, transactions + 1):
-        for name, value in random_inputs(ports).items():
+        stimuli = random_inputs(ports)
+        for name, value in stimuli.items():
             ports[name].value = value
         dut.ACT.value = 1
-        print(f'[INFO] Transaction inputs (#{i})')
-        print(f'        OP_CODE = {ports['OP_CODE'].value.integer}')
-        print(f'        MOVI    = {ports['MOVI'].value.integer}')
-        print(f'        REG_A   = {ports['REG_A'].value.integer}')
-        print(f'        REG_B   = {ports['REG_B'].value.integer}')
-        print(f'        MEM     = {ports['MEM'].value.integer}')
-        print(f'        IMM     = {ports['IMM'].value.integer}')
+
+        values = {'OP_CODE': stimuli['OP_CODE'],
+                  'REG_A': stimuli['REG_A']}
+        sample(values, coverpoints, crosses)
+
+        if log_transactions:
+            print(f'[INFO] Transaction inputs (#{i})')
+            print(f'        OP_CODE = {ports['OP_CODE'].value.integer}')
+            print(f'        MOVI    = {ports['MOVI'].value.integer}')
+            print(f'        REG_A   = {ports['REG_A'].value.integer}')
+            print(f'        REG_B   = {ports['REG_B'].value.integer}')
+            print(f'        MEM     = {ports['MEM'].value.integer}')
+            print(f'        IMM     = {ports['IMM'].value.integer}')
 
         for i in range(1, max_transaction_length + 1):
             await RisingEdge(dut.CLK)
             dut.ACT.value = 0
 
             if dut.DATA_VALID == 1:
-                print('[INFO] Transaction outputs:')
-                print(f'        DATA    = {dut.DATA.value.integer}')
                 break
 
             if i == max_transaction_length:
@@ -205,7 +246,13 @@ async def test_random_stimuli(dut: HierarchyObject) -> None:
                 print('[ERROR] Transaction did not finish in time.')
                 return
 
+            if log_transactions:
+                print('[INFO] Transaction outputs:')
+                print(f'        DATA    = {dut.DATA.value.integer}')
+
     stop.set()
 
     # To get coverage info, run and check newly created dir `logs`.
     # `verilator_coverage --annotate logs coverage.dat --annotate-min 1`
+
+    print_coverage(coverpoints, crosses)
